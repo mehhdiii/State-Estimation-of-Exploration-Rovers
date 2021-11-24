61eb010de0ed5bfd3e236ce1ad9e803984981f42
Mehdi <60067141+mehhdiii@users.noreply.github.com>
2021-11-24 20:41:04 +0500

    changes

--- sim_1.m
+++ sim_1.m
@@ -1,91 +1,102 @@
+ 
 clear; close all; clc;
-% define the system variables: 
+% define the system variables:
+ITER = 2000; %number of iterations
+T = 0.1; % Sampling time
+b = 0.4; % Platform width = 2b
 
 
 % define signal parameters: system
-var_v1 =1e-3; 
-var_v2 =1e-3; 
-var_v3 = 1e-3;
-var_v4 = 1e-3;
-var_v5 = 1e-3;
+var_v1 = 1e-4;
+var_v2 = 1e-4;
+var_v3 = 1e-6;
+var_v4 = 1e-6;
+var_v5 = 1e-6;
 
-Q_k = diag([var_v1 var_v2 var_v3 var_v4 var_v5]); 
+
+Q_k = diag([var_v1 var_v2 var_v3 var_v4 var_v5]);
+
 
 % define signal parameters: observation/sensor
-var_w1 = 1e-3;
-var_w2 = 1e-3; 
-W_k = diag([var_w1 var_w2]); 
-b = 1/2; % Platform width = 2b
+var_w1 = 1e-4;
+var_w2 = 1e-4;
+W_k = diag([var_w1 var_w2]);
 
-% Noise: 
-var_d1 =1e-3;
-var_d2 =1e-3; 
-D_k = diag([var_d1 var_d2]); 
 
-ITER = 10; %number of iterations
-[x, y] = get_path(ITER);
-T = 0.1;
-% x_last = [0; 0; 0; 5; 0.01]; %last value of states (X{k-1}): zero initially 
-xhat_last = [0; 0; 0; 0; 0]; %Last optimal predicted value (X_hat{k-1}): zero initially 
+x_k = [0;0;0;1;0]; % Plant initial state
+xhat_last = [0;0;0;0;0]; % Last optimal predicted value (X_hat{​​​​​​​k-1}​​​​​​​): zero initially
 P_last = eye(5); % Last covariance matrix value for the estimated states
 
-y_last = 0; %last observation: zero initially 
 
-%storage of values 
-historyX_k = [0; 0; 0; 0; 0]; 
-historyY_k = [0; 0];
-historyX_predict = [0; 0; 0; 0; 0]; 
+%storage of values
+historyX_k = zeros(5,1);
+historyY_k = zeros(2,1);
+historyX_pred = zeros(5,1);
 
-for k = 2:ITER   
+
+for k = 2:ITER
     
-    %generate noise values: 
-    v = sqrt(Q_k)*randn(5, 1); 
-    w = sqrt(W_k)*randn(2, 1); 
-    d = sqrt(D_k)*randn(2, 1); 
-    %previous states:
-    px = xhat_last(1); 
-    py = xhat_last(2); 
-%     phi = xhat_last(3) ;
-    phi = wrapToPi(atan2(py-y(k-1), px-x(k-1))); 
-    X_line = [x(k); y(k)] + d; 
-    [vx,omega] = new_get_velocities(xhat_last, X_line, T, phi);
-    disp([vx,omega]); drawnow
-    
-    %generate xk and yk: 
-    x_k = [
-        px + T*vx*cos(phi+T*omega/2); 
-        py + T*vx*sin(phi+T*omega/2); 
-        phi+T*omega; 
-        vx;
-        omega] + v; 
-    
-    y_k = [vx+b*omega; 
-        vx-b*omega] + w;
+    %generate noise values:
+    v = sqrt(Q_k)*randn(5,1);
+    w = sqrt(W_k)*randn(2,1);
+    %generate xk and yk:
+    vx = x_k(4);
+    omega = x_k(5);
+    phi = x_k(3);
+    x_k = x_k + [
+        T*vx*cos(phi+T*omega/2);
+        T*vx*sin(phi+T*omega/2);
+        T*omega;
+        0;
+        0] + v;
     x_k(3) = wrapToPi(x_k(3));
-    %run KF algorithm 
-    [xhat_optimal,P_optimal] = KalmanFilter(y_k, Q_k, W_k, xhat_last, P_last, T, b); 
-    xhat_optimal(3) = wrapToPi(xhat_optimal(3));
-    xhat_last = xhat_optimal; 
-    y_last = y_k; 
-    x_last = x_k; 
-    P_last = P_optimal; 
-    historyX_k(:, k) = x_k; 
-    historyY_k(:, k) = y_k; 
-    historyX_predict(:, k) = xhat_optimal; 
+    y_k = [0 0 0 1 b; 0 0 0 1 -b]*x_k + w;
+    
+    %run KF algorithm
+    [x_optimal,P_optimal] = ...
+        KalmanFilter(y_k,Q_k,W_k,xhat_last,P_last,T,b);
+    x_optimal(3) = wrapToPi(x_optimal(3));
+    
+    xhat_last = x_optimal;
+    P_last = P_optimal;
+    
+    historyX_k(:,k) = x_k;
+    historyY_k(:,k) = y_k;
+    historyX_pred(:, k) = x_optimal;
 end
 
-P = ITER-1; 
-figure()
-hold on 
 
-plot(x, y,'k-', 'LineWidth', 0.5 );
-plot(historyX_k(1, :), historyX_k(2, :), 'ko:');
-% plot(historyY_k(1, end-P:end).*cos(historyY_k(2, end-P:end)), historyY_k(1, end-P:end).*sin(historyY_k(2, end-P:end)), 'rx')
-plot(historyX_predict(1, :), historyX_predict(2, :), 'b--', 'LineWidth',2);
-xlabel("x coordinate", 'fontsize',12);
-ylabel("y coordinate", 'fontsize',12);
-title("Trajectory of Non-Linear system", 'fontsize',14);
-lgd = legend('Ideal Path', 'Noisy trajectory', 'Predicted trajectory', 'location', 'best');
-lgd.FontSize = 14;
-hold off;
-print -depsc results.eps;
+figure();hold on
+plot(historyX_k(1,:), historyX_k(2,:), 'kd:');
+plot(historyX_pred(1,:), historyX_pred(2,:), 'bs--', 'LineWidth',2);
+xlabel('x coordinate', 'fontsize',12);
+ylabel('y coordinate', 'fontsize',12);
+title('Comparing Trajectories', 'fontsize',14);
+lgd = legend('Stochastic trajectory',...
+    'Estimated trajectory', 'location', 'best');lgd.FontSize = 14;
+
+
+figure; plot(historyY_k(1,2:end),'k--');hold on
+plot(historyX_k(4,2:end)+b*historyX_k(5,2:end),'r-','linewidth',2)
+plot(historyX_pred(4,2:end)+b*historyX_pred(5,2:end),'b-','linewidth',2)
+title('Left-wheel Velocity unit/sec', 'fontsize',14);
+lgd = legend('Noisy observation','True Values','Estimated Values',...
+    'location', 'best');lgd.FontSize = 14;
+
+
+figure; plot(historyY_k(2,2:end),'k--');hold on
+plot(historyX_k(4,2:end)-b*historyX_k(5,2:end),'r-','linewidth',2)
+plot(historyX_pred(4,2:end)-b*historyX_pred(5,2:end),'b-','linewidth',2)
+title('Right-wheel Velocity unit/sec', 'fontsize',14);
+lgd = legend('Noisy observation','True values','Estimated values',...
+    'location', 'best');lgd.FontSize = 14;
+
+
+
+
+
+
+
+
+
+
